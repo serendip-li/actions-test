2fa0cfc077a7f15f6d8dd39ce109766639e35349
053568 <lisaipeng@citics.com>
2024-10-23 11:07:30 +0800

    modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I HAVE MADE A CHANGE!" />
+      <HelloWorld msg="I HAVE MADE A CHANGE..." />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
